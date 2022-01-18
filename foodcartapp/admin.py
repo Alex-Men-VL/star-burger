c68e57c02da5e28bc03996360fd0980927f32382
Aleksandr <menshikov.a.v@icloud.com>
2022-01-18 10:52:19 +0300

    Add a redirect to the orders page after successful editing

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.shortcuts import reverse
+from django.shortcuts import redirect, reverse
 from django.templatetags.static import static
+from django.utils.encoding import iri_to_uri
 from django.utils.html import format_html
+from django.utils.http import url_has_allowed_host_and_scheme
 
 from .models import Product
 from .models import ProductCategory
@@ -116,3 +118,12 @@ class OrderAdmin(admin.ModelAdmin):
     inlines = [
         OrderItemInline,
     ]
+
+    def response_post_save_change(self, request, obj):
+        if 'next' not in request.GET:
+            return super().response_post_save_change(request, obj)
+        if url_has_allowed_host_and_scheme(request.GET['next'], None):
+            url = iri_to_uri(request.GET['next'])
+            return redirect(url)
+        else:
+            raise
